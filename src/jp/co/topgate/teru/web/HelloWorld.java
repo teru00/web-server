cb1f35fe774b742e40787e555db9f52b7335d98f
terufumi shimoji <terufumishimoji@teru.local>
2016-11-22 09:42:55 +0900

    first commit

---
+++ src/jp/co/topgate/teru/web/HelloWorld.java
@@ -0,0 +1,10 @@
+package jp.co.topgate.teru.web;
+
+/**
+ * Created by terufumishimoji on 2016/11/22.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
