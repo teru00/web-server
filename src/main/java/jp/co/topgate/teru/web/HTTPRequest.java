5226fd3fe68426f37cb015322860530c24414e99
terufumi shimoji <terufumi.shimoji@topgate.co.jp>
2016-11-30 18:58:13 +0900

    getRequestMethod()追加

--- src/main/java/jp/co/topgate/teru/web/HTTPRequest.java
+++ src/main/java/jp/co/topgate/teru/web/HTTPRequest.java
@@ -1,7 +1,51 @@
 package jp.co.topgate.teru.web;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.BufferedReader;
+
+
+
 /**
  * Created by terufumishimoji on 2016/11/28.
  */
 public class HTTPRequest {
+    private InputStream inputStream;
+
+    HTTPRequest(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+    //getRequestMethod()
+    String getRequestMethod() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(this.inputStream));
+            String requestLine = br.readLine();
+            //requestLineから次のSPまで読み込む
+            //subString(int beginIndex, int lastIndex)を利用して、必要なtokenを抽出する
+            //beginIndexは0で、
+            String requestMethod = requestLine.substring(0, requestLine.indexOf(" "));
+            return requestMethod;
+        } catch (IOException e) {
+            //保留
+            System.err.println(e);
+        }
+    }
+    //getRequestURI()
+//    String getRequestMethod() {
+//        try {
+//            BufferedReader br = new BufferedReader(new InputStreamReader(this.inputStream));
+//            String requestLine = br.readLine();
+//            //requestLineから次のSPまで読み込む
+//            //subString(int biginIndex, int lastIndex)を利用して、必要なtokenを抽出する
+//            //biginIndexは0で、
+//            String requestMethod = requestLine.substring(0, requestLine.indexOf(" "));
+//            return requestMethod;
+//        } catch (IOException e) {
+//            //保留
+//            System.err.println(e);
+//        }
+//    }
+
 }
--- src/main/java/jp/co/topgate/teru/web/Server.java
+++ src/main/java/jp/co/topgate/teru/web/Server.java
@@ -1,35 +1,20 @@
 package jp.co.topgate.teru.web;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import jp.co.topgate.teru.web.HTTPRequest;
+
+
 /**
  * Created by terufumishimoji on 2016/11/22.
  */
 public class Server {
     public static void main(String[] args) {
-
-        ServerSocket serverSocket = null;
-        Socket socket = null;
-
-        //待機状態にして、接続要求があると、Client socketを取得する。
-        socket = serverSocket.accept();
-
-        br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        //brを使ってrequestを作っていく。
-
-        //handlerを初期化して
-        //responseを取得する。
-        HTTPRequestHandler handler = null;
-        HTTPRequest request = null;
-
-        response = handler.handle(request);
-        //responseをクライアントに送信する
-        response.send();
+        HTTPRequest request = new HTTPRequest(InputStream inputStream);
 
     }
 }
